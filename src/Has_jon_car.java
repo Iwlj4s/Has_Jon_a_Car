7617b2bfaca828becca4ba81c8dc19041328faa7
Iwlj4s <elizavetalenn@mail.ru>
2022-09-20 15:27:41 +0300

    Initial commit

---
+++ src/Has_jon_car.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+    public class Has_jon_car {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nHas Jon a car?\n");
+            System.out.println("(1) - Yes");
+            System.out.println("(2) - Nope");
+            System.out.println("(3) - Exit");
+            int selection = scan.nextInt();
+
+            if (selection == 3) {
+                System.out.println("Bye, faggot! =) ");
+                break;
+            }
+
+            if (selection == 1) {
+                System.out.println("Nope, U two fools without a car! ");
+                break;
+            }
+            else if (selection == 2);
+            System.out.println("Yeah! Jon is dup without a car!");
+            break;
+            }
+        }
+
+    }
